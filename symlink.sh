2df95e7e5b4ead5711cd86860a0ebfce707ae43f
zool <zool@zool-crunchbang>
2014-03-11 18:46:35 +0100

    updates script for compton.conf

--- symlink.sh
+++ symlink.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bash_aliases bashrc conkyrc mutt muttrc vim vimrc"    # list of files/folders to symlink in homedir
+files="bash_aliases bashrc conkyrc mutt muttrc vim vimrc compton.conf"    # list of files/folders to symlink in homedir
 
 ##########
 
